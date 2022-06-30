0da975fa90ed85f585aed3f9d34554f75ae209f7
will smith <willjohnsmithiii@gmail.com>
2022-06-30 15:25:05 -0600

    finished part 15 BUT it dosent look the same as tutorial because it wont let signout assuming because of the hardcoded value in _appjs for usercontextProvider. That should be fixed when we switch the param to userdata later on

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,11 @@
 import Link from 'next/link' ;
-
+import { useContext } from 'react';
+import { UserContext } from '../lib/context'
 
 // top navbar
 export default function Navbar() {
-    const user = null;
-    const username = null;
-    
+
+    const { user, username } = useContext(UserContext);    
     return (
 
         <nav className="navbar">
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
-
+import { UserContext } from '../lib/context';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <UserContext.Provider value={{ user:{}, username: 'will' }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
+      </UserContext.Provider>
+
     </>
   );
 }
