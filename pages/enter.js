09f74c89bbbfe0a28fd36bd9bde17236b91d5f3a
will smith <willjohnsmithiii@gmail.com>
2022-06-30 14:28:49 -0600

    done with part 14. google signin. it signs in on the /enter page but dosent do anything with the users .json file yet. i had to deviate from tutroial to use firebase version 9 because the tutorial uses outdated version 8

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,44 @@
-export default function EnterPage({ }) {
+import { signInWithPopup, signOut } from 'firebase/auth';
+import { auth, googleAuthProvider } from '../lib/firebase';
+
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
+  
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
   return (
-    <main>
-                <h1>Sign Up</h1>
-    </main>
+      <main> 
+        {user ? 
+        ! username ? <UsernameForm /> : <SignOutButton /> 
+        :
+        <SignInButton />
+        }
+      </main>
   )
+}
+
+// Sign in With Google Button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, googleAuthProvider);
+    };
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign In With Google
+    </button>
+
+    );
+  }
+
+// Sign Out Button 
+function SignOutButton() {
+  return <button onClick ={() => signOut(auth)}>Sign Out</button>
+}
+
+function UsernameForm() {
+
 }
